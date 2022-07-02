d91cf16978101997e97e5d32e338e6e8d2fd8d19
Matt Pike <mrpike@users.noreply.github.com>
2022-07-02 19:58:06 +0800

    Cleanup and documentation added

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# orcid-to-bibtex
-A simple command line tool which converts an individual's ORCID "Works" to BibTeX
+# ORCID to BibTeX
+Command-line program which generates a BibTeX file from an individual's ORCID record.
+
+## Features
+
++ Concurrent downloading of works from ORCID using [AIOHTTP](https://docs.aiohttp.org/en/stable/)
++ Intelligent renaming of BibTeX keys (to avoid duplicates) using keyword extraction ([YAKE](https://github.com/LIAAD/yake))on work titles
+
+
+## Usage
+Clone this repository to your local machine using git:
+```shell
+git clone https://github.com/MrPike/orcid-to-bibtex.git
+```
+Navigate into directory containing the code:
+```shell
+cd orcid-to-bibtex
+```
+Now install the application's dependencies. It's recommended that you use  [Poetry](https://python-poetry.org) to do this.
+```shell
+poetry install
+```
+
+Now activate a poetry shell and run the application:
+
+```shell
+poetry shell
+python3 orcid-to-bibtex.py 0000-0000-0000-1234
+```
+
+Replace *0000-0000-0000-1234*,  with the ORCID id of the individual whose works you would like to retrieve.
+
+The application supports the following arguments:
+
+```
+-o          The output path for the generated BibTeX file. A value must be specified.
+--dl        The maximum number of concurrent connections to ORCID's API. The default is 50.
+--orderby   How BibTeX entries should be sorted. Multiple fields can be specified. e.g. year title. The default is 'id'.
+--indent    The number of spaces each field in a given entry should be indented by. The default is 4 spaces.
+--ssl       Indicates whether SSL certificates should be validated or not. Default is true (meaning - certificates are validated).
+```
+
+### Example usages:
+
+1. Get all works for the ORCID user with the ID '0000-0002-1543-0148' and save the file to 'my_pubs.bib':
+
+```shell
+python3 orcid-to-bibtex.py 0000-0002-1543-0148 -o my_pubs.bib
+```
+
+2. As with \#1 (above), but entry details should be indented by 8 spaces (instead of the default - 4) and entries sorted by publication year, then title:
+
+```shell
+python3 orcid-to-bibtex.py 0000-0002-1543-0148 -o my_pubs.bib --indent 8 --orderby year title
+```
--- orcid-to-bibtex.py
+++ orcid-to-bibtex.py
@@ -1,15 +1,23 @@
+import json
 from collections import defaultdict
-
 from argparse import Namespace, ArgumentParser
-from typing import Any
 from asyncio import Semaphore, run, gather
+
 from aiohttp import ClientSession, TCPConnector
 from pathlib import Path
 import bibtexparser as bp
 import yake
 
 
-async def get_orcid(orcid_path: str, session: ClientSession, dl_limit: Semaphore) -> Any:
+async def get_orcid(orcid_path: str, session: ClientSession, dl_limit: Semaphore) -> json:
+    """
+    Retrieves a single item from the ORCID API endpoint.
+    :param orcid_path: The ORCID API path (not include base domain) for the desired item
+    :param session: AIOHTTP session for the application
+    :param dl_limit: Semaphore controlling the number of simultaneous connections to the ORCID API
+    :return: JSON response returned from the ORCID API
+    """
+    # TODO - add error handling
     async with dl_limit:
         async with session.get(
                 f"https://pub.orcid.org/{orcid_path}",
@@ -18,19 +26,28 @@ async def get_orcid(orcid_path: str, session: ClientSession, dl_limit: Semaphore
             return await response.json()
 
 
-async def download_all(urls: list, session: ClientSession, dl_limit: Semaphore) -> Any:
-    return await gather(*[get_orcid(url, session, dl_limit) for url in urls])
+async def get_orcid_works(orcid_id: str, max_dls: int = 50, validate_ssl: bool = True) -> list[str]:
+    """
+    Retrieves all works associated with the provided ORCID ID. Returns a string in BibTeX format. Note,
+    that the resulting BibTeX returned by this function will likely contain duplicate keys.
 
-
-async def get_orcid_works(orcid_id: str) -> Any:
-    dl_limit = Semaphore(50)
-    async with ClientSession(connector=TCPConnector(ssl=False)) as session:
+    :param orcid_id: ORCID ID of the user's whose works are to be retrieved.
+    :param max_dls: Maximum concurrent download workers made to ORCID's API.
+    :param validate_ssl: Whether SSL certificates should be validated.
+    :return: A collection of BibTeX strings for each work retrieved from ORCID. Contains duplicate keys.
+    """
+    dl_limit = Semaphore(max_dls)
+    async with ClientSession(connector=TCPConnector(ssl=validate_ssl)) as session:
+        # Get the list of all user's works
         works = await get_orcid(f"{orcid_id}/works", session, dl_limit)
         urls = []
+        # For each work, generate the API path needed in order to retrieve all details
         for work in works['group']:
             urls.append(work["work-summary"][0]["path"])
-        results = await download_all(urls, session, dl_limit)
+        # Get details for all works
+        results = await gather(*[get_orcid(url, session, dl_limit) for url in urls])
         bib = []
+        # Extract BibTeX provided by ORCID
         for work in results:
             if work['citation']['citation-type'] == 'bibtex':
                 bib.append(work['citation']['citation-value'])
@@ -38,53 +55,85 @@ async def get_orcid_works(orcid_id: str) -> Any:
         return bib
 
 
-def parse_and_format_bib(input_bib: Path, out_bib: Path, indent: int = 4, order_by: str | tuple = 'year') -> None:
-    db = bp.loads(input_bib.read_text())
+def parse_and_format_bib(input_bib: str, indent: int = 4, order_by: tuple = ('id',)) -> None:
+    """
+    Parses and formats BibTeX, writes it to file. Intelligently renames BibTeX IDs according to each entry's title,
+    using keyword extraction.
+    :param input_bib: String containing non-formatted (and likely duplicate keys) BibTeX
+    :param out_bib: The formatted BibTeX string
+    :param indent: Formatting option: number of spaces to indent each entry's fields
+    :param order_by: Formatting option: fields by which, entries should be ordered
+    """
+
+    db = bp.loads(input_bib)
+
+    # Dict is used to count and check for duplicate keys in the generated BibTeX file
     bib_id_count = defaultdict(int)
-
     for e in db.entries:
-        title = ''.join([l for l in e['title'] if l.isalpha() or l.isspace()])
+        # Remove all non-alphanumeric characters from the title, apart from spaces
+        title = ''.join([character for character in e['title'] if character.isalpha() or character.isspace()])
         keywords = yake.KeywordExtractor().extract_keywords(title)
-        id = e['ID']
+        bibtex_id = e['ID']
 
-        unique = False
-        c = 0
-
+        # Intelligent renaming of BibTeX entry keys based on the title of the work.
+        # Keywords extracted from the title are added to the key until a unique key is generated. If keywords are
+        # exhausted and the resulting key is still not unique, then the instance number for that key is appended.
+        unique, c = False, 0
         while not unique:
             if c < len(keywords):
-                id += '_' + keywords[c][0].replace(' ', '_').title()
-                if id not in bib_id_count:
-                    bib_id_count[id] += 1
+                bibtex_id += '_' + keywords[c][0].replace(' ', '_').title()
+                if bibtex_id not in bib_id_count:
+                    bib_id_count[bibtex_id] += 1
                     unique = True
             else:
-                bib_id_count[id] += 1
-                id += id + '_' + str(bib_id_count)
+                bib_id_count[bibtex_id] += 1
+                bibtex_id += bibtex_id + '_' + str(bib_id_count)
 
-        e['ID'] = id
-        print(e['ID'])
+        e['ID'] = bibtex_id
 
+    # Write the formatted BibTeX to file
     writer = bp.bwriter.BibTexWriter()
     writer.indent = ' ' * indent  # indent entries with
     writer.order_entries_by = order_by
-    out_bib.write_text(writer.write(db))
+
+    return writer.write(db)
 
 
 def parse_cli_args() -> Namespace:
+    """
+    Argument parser for the application
+    :return: An object containing the user specified arguments
+    """
     p = ArgumentParser(description='Generates a BibTeX file for a given ORCID id.')
     p.add_argument('ORCID', type=str, metavar='0000-0000-0000-0000',
-                   help="The ORCID ID for the individual whose works should be recorded.")
+                   help="Individual's ORCID ID.")
     p.add_argument('-o', type=Path, metavar='PATH',
-                   help="The destination for the output BibTeX file.")
-    return p.parse_args()
+                   help="The output path of the generated BibTeX file.")
+    p.add_argument('--dl', type=int, metavar='MAX_DL', default=50,
+                   help="The maximum number of concurrent connections to ORCID's servers.")
+    p.add_argument('--orderby', type=str, nargs='+', metavar='ORDER_BY', default='year',
+                   help='How entries should be ordered/sorted. Default = Order by year of publication.')
+    p.add_argument('--indent', type=int, metavar='INDENT', default=4,
+                   help='How many spaces should each field be indented by?')
+    p.add_argument('--ssl', type=bool, metavar='VALIDATE_SSL', default=True,
+                   help="Validate SSL certificates when connecting to ORCID's API.")
+
+    args = p.parse_args()
+    if args.o is None:
+        args.o = Path(args.ORCID + '.bib')
+
+    if args.orderby is None:
+        args.orderby = ('id',)
+    else:
+        args.orderby = tuple(args.orderby)
+    return args
 
 
-async def main():
+async def main() -> None:
     args = parse_cli_args()
-    orcid = args.ORCID  # Test with: '0000-0002-1543-0148'
-    bib_path = args.o or Path(f'{orcid}.bib')
-    bib = await get_orcid_works(orcid)
-    bib_path.write_text('\n'.join(bib))
-    parse_and_format_bib(bib_path, bib_path)
+    bib = ''.join(await get_orcid_works(args.ORCID, max_dls=args.dl, validate_ssl=args.ssl))
+    args.o.write_text(parse_and_format_bib(bib, indent=args.indent, order_by=args.orderby))
 
 
-run(main())
+if __name__ == '__main__':
+    run(main())
